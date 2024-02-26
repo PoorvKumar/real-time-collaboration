e99b41b56a655fbf8d9b1e4e5759139df8635f95
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2024-02-26 20:19:05 +0530

    client directory

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/real-time-collaboration/",
+  base: "/real-time-collaboration/client",
   plugins: [react()],
   resolve: {
     alias: {
